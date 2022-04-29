e795bae1690ae964f61d9907394fd8521734aaba
dimitar.nizamov <dimitur2204@gmail.com>
2022-04-29 10:35:32 +0200

    Add password authentication to be able to access the pages

---
+++ password.js
@@ -0,0 +1,13 @@
+const PASSWORD_TO_LOG_IN = 'socialmediaiskey'
+const $form = document.getElementById('pass-form')
+const $password = document.getElementById('password')
+const $validationError = document.getElementById('validation-password')
+$form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if ($password.value !== PASSWORD_TO_LOG_IN) {
+        $password.classList.add('is-invalid')
+        $validationError.style.display = 'block'
+    } else {
+        window.location.href = 'index.html';
+    }
+})
